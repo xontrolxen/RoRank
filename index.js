f73e4b700ccf138f7365a304929b8506746b6629
Xon <urfav.soulz@gmail.com>
2024-06-28 11:35:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const { prefix, token, cookie, color } = require('./config.json'); // Get your token, prefix and cookie from here..
  const nblx = require("noblox.js"); // The module of roblox..
-var groupId = 7445909 // Your group id
+var groupId = 34269583 // Your group id
 
 // To change the permissions go to line 30, 56, 99 ,127, 155, 176
 
